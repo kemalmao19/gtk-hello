0632f61ee45ae25fcc04bafb965bbe780d6a4753
kemalmao19 <sebelumtidur14@gmail.com>
2024-10-17 22:59:36 +0700

    add env

---
+++ flake.nix
@@ -0,0 +1,22 @@
+{
+  description = "vala dev environment for kemal gtk development learning";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in with pkgs; {
+        devShells.default = mkShell {
+          buildInputs = [
+            vala
+            vala-lint
+            vimPlugins.nvim-treesitter-parsers.vala
+            vala-language-server
+          ];
+        };
+      });
+}
